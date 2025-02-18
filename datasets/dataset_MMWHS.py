a329a64d729831a77603fab7804ad1c8c5d9257c
xxx <67528991+Asphyxiate-Rye@users.noreply.github.com>
2025-02-18 10:55:26 +0800

    Update dataset_MMWHS.py

--- datasets/dataset_MMWHS.py
+++ datasets/dataset_MMWHS.py
@@ -124,33 +124,22 @@ def random_elastic(image,label, alpha, sigma,
 
     shape = image.shape
     shape_size = shape[:2]
-    # Random affine
     center_square = np.float32(shape_size) // 2
     square_size = min(shape_size) // 3
-    # pts1: 仿射变换前的点(3个点)
     pts1 = np.float32([center_square + square_size,
                        [center_square[0] + square_size,
                         center_square[1] - square_size],
                        center_square - square_size])
-    # pts2: 仿射变换后的点
     pts2 = pts1 + random_state.uniform(-alpha_affine, alpha_affine,
                                        size=pts1.shape).astype(np.float32)
-    # 仿射变换矩阵
     M = cv2.getAffineTransform(pts1, pts2)
-    # 对image进行仿射变换.
     imageB = cv2.warpAffine(image, M, shape_size[::-1], borderMode=cv2.BORDER_REFLECT_101)
     labelB = cv2.warpAffine(label, M, shape_size[::-1], borderMode=cv2.BORDER_REFLECT_101)
 
-    # generate random displacement fields
-    # random_state.rand(*shape)会产生一个和shape一样打的服从[0,1]均匀分布的矩阵
-    # *2-1是为了将分布平移到[-1, 1]的区间, alpha是控制变形强度的变形因子
     dx = gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma) * alpha
     dy = gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma) * alpha
-    # generate meshgrid
     x, y = np.meshgrid(np.arange(shape[1]), np.arange(shape[0]))
-    # x+dx,y+dy
     indices = np.reshape(y + dy, (-1, 1)), np.reshape(x + dx, (-1, 1))
-    # bilinear interpolation
     imageC = map_coordinates(imageB, indices, order=1, mode='constant').reshape(shape)
     labelC = map_coordinates(labelB, indices, order=1, mode='constant').reshape(shape)
 
